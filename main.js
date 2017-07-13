c5ec70cf77da2b4d08248deb3bf1966a494bc5e9
FireMario211 <firemario07@gmail.com>
2017-07-12 22:48:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 /**
-
       SmartHouse
     By FireMario211
   For The House server
 
 **/
+const discord = require('discord.js')
+const client = new discord.Client()
+const settings = require('./settings.json')
+
+client.on('ready' () => {
+	console.log("Ready")
+})
+
+client.login(settings.token)
