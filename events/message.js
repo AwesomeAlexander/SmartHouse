b839c4cef2807c6b2ad9b4dccf4fa49a58569354
Mario <firemario07@gmail.com>
2017-08-12 15:45:49 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,22 @@
 const settings = require('../settings.json');
+const fs = require('fs')
+const discord = require('discord.js')
+var stories = JSON.parse(fs.readFileSync("./jsondb/checkstory.json", "utf8"));
+var kkkkkk = stories.find(function(rolez){ 
+    return rolez.iddd === 42
+})
 module.exports = message => {
   let client = message.client;
   if (message.author.bot) return;
+  if(message.channel.id === '345660187748728832'){
+      if(!message.content.startsWith(settings.prefix)){
+        if(kkkkkk.storyoff === 1) return message.reply("Sorry but one-word stories is off! Please wait until the bot restarts!")
+      fs.appendFile('./jsondb/stories.txt', message.content + " ")
+        return;
+      
+      }
+
+  }
   if (!message.content.startsWith(settings.prefix)) return;
   let command = message.content.split(' ')[0].slice(settings.prefix.length);
   let params = message.content.split(' ').slice(1);
