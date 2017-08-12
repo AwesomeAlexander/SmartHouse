9516bf8ec6586d8de88a92252e78f3c72d63e68d
Alexander <AwesomeAlexander@users.noreply.github.com>
2017-08-12 14:31:40 -0700

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,4 +1,5 @@
 module.exports = (guild) => {
-	guild.defaultChannel.send("This bot is private meaning its not allowed to invite!")
-	guild.leave()
+	guild.defaultChannel.send("This bot is private, so you can't invite it anywhere else, sorry!");
+	guild.defaultChannel.send("If you'd like to join the server it's on, go to http://www.enter-the-house.com");
+	guild.leave();
 }
